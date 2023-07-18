c04b4d0a9261ea96bc37ea3bd34217d31f3fcf22
Asiphenombeleko <asiphenombeleko33@gmail.com>
2023-07-18 12:28:09 +0200

    initial commit

--- greet_xola.js
+++ greet_xola.js
@@ -1,6 +1,7 @@
-export default function greet(name){
-    return "Hello, " + name + " - from ES modules";
-};
+import chalk from 'chalk';
+//import the greet module that is in the current folder
+import greet from './greet.js'
 
-// call the function & print the result on the screen
-console.log(greet('Xola'))
+
+const styledMessage = chalk.bgGreen.black(greet('Xola'));
+console.log(styledMessage)
