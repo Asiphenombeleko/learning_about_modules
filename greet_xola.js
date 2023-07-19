89d82234357cd4a3d9c9a6dcc531baaf94af8f46
Asiphenombeleko <asiphenombeleko33@gmail.com>
2023-07-19 11:11:21 +0200

    feedback changes

--- greet_xola.js
+++ greet_xola.js
@@ -1,8 +1,28 @@
 import chalk from 'chalk';
 //import the greet module that is in the current folder
 import greet from './greet.js'
+import figlet from 'figlet';
 
 
 const styledMessage = chalk.bgGreen.black(greet('Xola'));
-
 console.log(styledMessage)
+
+figlet.text(
+    "Asi!",
+    {
+      font: "Ghost",
+      horizontalLayout: "default",
+      verticalLayout: "default",
+      width: 80,
+      whitespaceBreak: true,
+    },
+    function (err, data) {
+      if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+      }
+      console.log(data);
+    }
+  );
+  
